8b4d99b28b788307238696aa423906c7b69ab825
Jose <solorzano202009@gmail.com>
2018-03-01 15:32:52 -0400

    Actualizado el contenido de IOT - Internet Of Things.

--- application/language/english/smartindustries_lang.php
+++ application/language/english/smartindustries_lang.php
@@ -5,60 +5,29 @@ $lang['smartindustries_page_title1_span'] = 'Innovative Solutions for differenti
 $lang['smartindustries_page_title1_li1'] = 'Home';
 $lang['smartindustries_page_title1_li2'] = 'IOT - INTERNET OF THINGS';
 
+// Sección 1
+$lang['smartindustries_heading_block0_h1'] = 'The BEE2 Concept';
+$lang['smartindustries_heading_block0_p1'] = 'Like the honeycombs in a hive, Compta’s vision stands for a world where your different IoT applications can co-exist, interact and collaborate in the same environment, breaking down silos and taking the max advantage of the information gathered from several services. Compta’s BEE2 Verticals Aggregation Layer and BEE2 Product Family materializes that vision:';
+$lang['smartindustries_heading_block0_li1'] = 'Your IoT Services, all connected to the same “big data” backend.';
+$lang['smartindustries_heading_block0_li2'] = 'Easily obtain information from the several IoT services and generate unified reports from the same unique platform.';
+$lang['smartindustries_heading_block0_li3'] = 'Use the same intuitive interface across the different services and get your telemetering, perform analytics, generate alarms, manage users and sensors and create your automation rules.';
+
 // Sección 2
-$lang['smartindustries_page_title2_h1_2'] = 'VERTICAL SEGMENT IT SOLUTIONS';
-$lang['smartindustries_page_title2_p'] = 'With a significant track-record of live implementations, industry 4.0 vertical solutions are designed to help boosting your business by reducing costs, increasing efficiency and maximize gains.';
+$lang['smartindustries_heading_block1_h1'] = 'Many IoT Services. Same Interface';
+$lang['smartindustries_heading_block1_img1'] = 'smartindustries-section2-img1.jpg';
+$lang['smartindustries_heading_block1_title1'] = 'BEE2 Verticals Aggregation Layer';
+$lang['smartindustries_heading_block1_p1'] = 'All “BEE2” service applications sahre the same aggregation layer providing significant advantages, such as:';
+$lang['smartindustries_heading_block1_li1'] = 'Unified Management Interface for the several applications';
+$lang['smartindustries_heading_block1_li2'] = 'Single Sign-On for all the verticals';
+$lang['smartindustries_heading_block1_li3'] = 'Unified Reporting, Monitoring and Alarms';
+$lang['smartindustries_heading_block1_li4'] = 'Predictive Analytics';
+$lang['smartindustries_heading_block1_li5'] = 'Common Rules Engine for the several verticals';
+$lang['smartindustries_heading_block1_li6'] = 'Events Correlation across the different services';
+$lang['smartindustries_heading_block1_li7'] = 'Easy API integration with external IoT platforms & other modules';
 
-// Sección de video
-$lang['smartindustries_parallax_video1'] = 'A wide range of Services and Solutions for your Business.';
-
-// Sección 4
-$lang['smartindustries_page_title3_h2'] = 'INNOVATE TODAY YOUR BUSINESS';
-$lang['smartindustries_page_title3_h3_1'] = 'Energy Consumption Management';
-$lang['smartindustries_page_title3_h3_2'] = 'Urban Solid Waste Collection Management';
-$lang['smartindustries_page_title3_h3_3'] = 'Control System for Irrigation';
-$lang['smartindustries_page_title3_h3_4'] = 'Irrigation Pivots Control';
-$lang['smartindustries_page_title3_h3_5'] = 'Vehicles Fleet Management';
-$lang['smartindustries_page_title3_h3_6'] = 'Railway Cargo Management';
-$lang['smartindustries_page_title3_h3_7'] = 'Cargo Terminals Management';
-$lang['smartindustries_page_title3_h3_8'] = 'Aquaculture Production Management';
-$lang['smartindustries_page_title3_p1'] = 'The Bee2Energy solution addresses the need to improve Energy Consumption Management and electronics. Applicable to all public services such as: public lighting, electricity, gas, water and oxygen.';
-$lang['smartindustries_page_title3_p2'] = 'Bee2Waste offers the most complete software solution to operate, monitor, optimize, control and report through all phases of the waste collection and processing process. Bee2Waste focuses on modulating software that enables business growth and is available in both cloud-based and premise-based versions.';
-$lang['smartindustries_page_title3_p3'] = 'Remote Irrigation Management for Sprinkler, Rain Guns and Drip Irrigation Systems. Applicable both for agriculture, sport stadiums, parks and urban green spaces. Irrigation, Liquid Fertilization and environmental control.';
-$lang['smartindustries_page_title3_p4'] = 'Remote Management and Control for Irrigation Pivots. Irrigation, Fertigation, weather predictive control and integration with agricultural sensing equipment’s. Supports pivots from several vendors and types (center, lateral move and nozzle pivot types).';
-$lang['smartindustries_page_title3_p5'] = 'Much more than tracking. “Bee2Fleet” solution covers from Tracking, Geo-Fencing, Routes Optimization, Fuel Management, Maintenance & Repair module, Drivers scheduling, Tolls & Fines management, Insurance contracts & Accidents management and Alarms & Events correlation.';
-$lang['smartindustries_page_title3_p6'] = 'Management Software suite for multimodal transport and rail, with a 360º view of all cargo operations. Covers Operational Management, Equipment’s Registration Module, Contract’s Management Module, Human Resource Management, Billing, Documents Management and Reporting Modules.';
-$lang['smartindustries_page_title3_p7'] = 'Total Cargo Management Operations for both Marine Ports & In-land container terminals. Container Control, Yard Management, Ship Planning cargo load, Vessel Operations, Terminal Management, Reefer Control, Berth Planning, Automatic Billing and complete set of Reporting';
-$lang['smartindustries_page_title3_p8'] = 'Compta’s Aquatrololis emerges to empower the aquaculture sector with technological advances that allow the intelligent control of the entire production cycle, optimizing production processes in line with market behavior. Available Modules: SmartMarket (B2B & B2C species marketplace), SmartFarm (Base & Traceability), SmartEnvironment, SmartPower.';
-
-// Sección 5 (Tabla)
-$lang['smartindustries_section_features_h2'] = 'MODULES, APPLICABILITY & IMPACTS';
-$lang['smartindustries_section_features_span'] = 'for Sustainability and Efficiency Goals';
-$lang['smartindustries_table_th1'] = 'Solution Modules';
-$lang['smartindustries_table_th2'] = 'Featured Products';
-$lang['smartindustries_table_th3'] = 'Areas of Applicability';
-$lang['smartindustries_table_th4'] = 'Energy Savings';
-$lang['smartindustries_table_th5'] = 'Operational Savings';
-$lang['smartindustries_table_tr1_1'] = 'Energy Consumption <br>Management';
-$lang['smartindustries_table_tr2_1'] = 'Green Spaces <br>Management';
-$lang['smartindustries_table_tr3_1'] = 'Waste Collection <br>Management';
-$lang['smartindustries_table_tr4_1'] = 'Agriculture';
-$lang['smartindustries_table_tr1_3_1'] = 'Public Lighting';
-$lang['smartindustries_table_tr1_3_2'] = 'Exterior Lighting';
-$lang['smartindustries_table_tr1_3_3'] = 'Office Buildings and Campuses';
-$lang['smartindustries_table_tr1_3_4'] = 'Condominiums';
-$lang['smartindustries_table_tr1_3_5'] = 'Museums, Theaters and';
-$lang['smartindustries_table_tr1_3_6'] = 'Arenas';
-$lang['smartindustries_table_tr1_3_7'] = 'Swimming Pools';
-$lang['smartindustries_table_tr2_3_1'] = 'Parks & Gardens';
-$lang['smartindustries_table_tr2_3_2'] = 'Lakes';
-$lang['smartindustries_table_tr2_3_3'] = 'Golf Courses';
-$lang['smartindustries_table_tr2_3_4'] = 'Universities';
-$lang['smartindustries_table_tr2_3_5'] = 'Condominiums';
-$lang['smartindustries_table_tr3_3_1'] = 'Truck Route Optimization';
-$lang['smartindustries_table_tr3_3_2'] = 'Machine predictive control';
-$lang['smartindustries_table_tr3_3_3'] = 'PAYT';
-$lang['smartindustries_table_tr4_3_1'] = 'Pivot remote control';
-$lang['smartindustries_table_tr4_3_2'] = 'Drips & Sprinklers';
-$lang['smartindustries_table_tr4_3_3'] = 'Boreholes & Pumps';
-$lang['smartindustries_table_tr4_3_4'] = 'Multi-crop';
+// Sección 3
+$lang['smartindustries_heading_block2_h1'] = 'BEE2 Product Family';
+$lang['smartindustries_heading_block2_img1'] = 'smartindustries-section3-img1.jpg';
+$lang['smartindustries_heading_block2_title1'] = 'Smart Cities and Industry Specific Verticals';
+$lang['smartindustries_heading_block2_p1'] = 'BEE2 Product Family is comprised today by 6 (six) IoT Vertical Applications: Bee2Energy, Bee2Lighting, Bee2Waste, Bee2Green, Bee2Crop and Bee2FireDetection, plus 1 (one) Service Application for Citizens: Bee2Citizen in form of a Mobile and Web App.';
+$lang['smartindustries_heading_block2_p2'] = 'The offer covers the most popular areas of interest for Smart City projects such as energy management, street lighting, urban waste collection, green spaces management and citizens plus other industry specific solutions for Agriculture and Forests such as smart irrigation and forest fire detection.';
--- application/language/spanish/smartindustries_lang.php
+++ application/language/spanish/smartindustries_lang.php
@@ -1,64 +1,33 @@
 <?php
 // Sección de título
-$lang['smartindustries_page_title1_h1'] = '4.0 INDUSTRIA';
-$lang['smartindustries_page_title1_span'] = 'Soluciones innovadoras para mercados diferenciados';
-$lang['smartindustries_page_title1_li1'] = 'Inicio';
-$lang['smartindustries_page_title1_li2'] = '4.0 INDUSTRIA';
+$lang['smartindustries_page_title1_h1'] = 'IOT - INTERNET OF THINGS';
+$lang['smartindustries_page_title1_span'] = 'Innovative Solutions for differentiated Markets';
+$lang['smartindustries_page_title1_li1'] = 'Home';
+$lang['smartindustries_page_title1_li2'] = 'IOT - INTERNET OF THINGS';
+
+// Sección 1
+$lang['smartindustries_heading_block0_h1'] = 'The BEE2 Concept';
+$lang['smartindustries_heading_block0_p1'] = 'Like the honeycombs in a hive, Compta’s vision stands for a world where your different IoT applications can co-exist, interact and collaborate in the same environment, breaking down silos and taking the max advantage of the information gathered from several services. Compta’s BEE2 Verticals Aggregation Layer and BEE2 Product Family materializes that vision:';
+$lang['smartindustries_heading_block0_li1'] = 'Your IoT Services, all connected to the same “big data” backend.';
+$lang['smartindustries_heading_block0_li2'] = 'Easily obtain information from the several IoT services and generate unified reports from the same unique platform.';
+$lang['smartindustries_heading_block0_li3'] = 'Use the same intuitive interface across the different services and get your telemetering, perform analytics, generate alarms, manage users and sensors and create your automation rules.';
 
 // Sección 2
-$lang['smartindustries_page_title2_h1_2'] = 'SEGMENTO VERTICAL SOLUCIONES DE TI';
-$lang['smartindustries_page_title2_p'] = 'Con una importante trayectoria de implementaciones en vivo, las soluciones verticales de Industry 4.0 están diseñadas para ayudar a impulsar su negocio reduciendo costos, aumentando la eficiencia y maximizando las ganancias.';
+$lang['smartindustries_heading_block1_h1'] = 'Many IoT Services. Same Interface';
+$lang['smartindustries_heading_block1_img1'] = 'smartindustries-section2-img1.jpg';
+$lang['smartindustries_heading_block1_title1'] = 'BEE2 Verticals Aggregation Layer';
+$lang['smartindustries_heading_block1_p1'] = 'All “BEE2” service applications sahre the same aggregation layer providing significant advantages, such as:';
+$lang['smartindustries_heading_block1_li1'] = 'Unified Management Interface for the several applications';
+$lang['smartindustries_heading_block1_li2'] = 'Single Sign-On for all the verticals';
+$lang['smartindustries_heading_block1_li3'] = 'Unified Reporting, Monitoring and Alarms';
+$lang['smartindustries_heading_block1_li4'] = 'Predictive Analytics';
+$lang['smartindustries_heading_block1_li5'] = 'Common Rules Engine for the several verticals';
+$lang['smartindustries_heading_block1_li6'] = 'Events Correlation across the different services';
+$lang['smartindustries_heading_block1_li7'] = 'Easy API integration with external IoT platforms & other modules';
 
-// Sección de video
-$lang['smartindustries_parallax_video1'] = 'UNA AMPLIA GAMA DE SERVICIOS Y SOLUCIONES PARA SU NEGOCIO.';
-
-// Sección 4
-$lang['smartindustries_page_title3_h2'] = 'INNOVA HOY TU NEGOCIO';
-$lang['smartindustries_page_title3_h3_1'] = 'GESTIÓN DEL CONSUMO DE ENERGÍA';
-$lang['smartindustries_page_title3_h3_2'] = 'GESTIÓN DE LA RECOLECCIÓN DE RESIDUOS SÓLIDOS URBANOS';
-$lang['smartindustries_page_title3_h3_3'] = 'SISTEMA DE CONTROL DE RIEGO';
-$lang['smartindustries_page_title3_h3_4'] = 'CONTROL DE PIVOTES DE RIEGO';
-$lang['smartindustries_page_title3_h3_5'] = 'MANEJO DE LA FLOTA DE LOS VEHICULOS';
-$lang['smartindustries_page_title3_h3_6'] = 'GESTIÓN DE CARGA FERROVIARIA';
-$lang['smartindustries_page_title3_h3_7'] = 'GESTIÓN DE TERMINALES DE CARGA';
-$lang['smartindustries_page_title3_h3_8'] = 'GESTIÓN DE LA PRODUCCIÓN DE ACUICULTURA';
-$lang['smartindustries_page_title3_p1'] = 'La solución Bee2Energy, permite abordar la necesidad de mejorar la Gestión del Consumo de Energía y la electrónica. Aplicable a todos los servicios públicos tales como: iluminación pública, electricidad, gas, agua y oxígeno.';
-$lang['smartindustries_page_title3_p2'] = 'Bee2Waste, ofrece la solución de software más completa para operar, monitorear, optimizar, controlar y reportar a lo largo de todas las fases del proceso de recolección y procesamiento de residuos. Bee2Waste se enfoca en modular los software que permitan el crecimiento de negocio y está disponible tanto en versiones basadas en la nube como en premisas.';
-$lang['smartindustries_page_title3_p3'] = 'Gestión remota de riego para rociadores, pluviales y sistemas de riego por goteo. Aplicable tanto para la agricultura, estadios deportivos, parques y espacios verdes urbanos. Riego, fertilización líquida y control ambiental.';
-$lang['smartindustries_page_title3_p4'] = 'Gestión remota y control de pivotes de riego. Irrigación, fertirrigación, control predictivo del tiempo e integración con equipos de detección agrícola. Soporta pivotes de varios vendedores y tipos (centro, movimientos laterales y tipos de pivotes de boquillas).';
-$lang['smartindustries_page_title3_p5'] = 'Mucho más que seguir. La solución "Bee2Fleet" abarca desde seguimiento, geo-esgrima, optimización de rutas, gestión de combustible, mantenimiento y reparación de módulos, programación de controladores, gestión de peajes y multas, contratos de seguros y gestión de accidentes y correlación de alarmas y eventos.';
-$lang['smartindustries_page_title3_p6'] = 'Contamos con el Software de gestión Suite para transporte multimodal y ferroviario, con una visión de 360º de todas las operaciones de carga. Cubre Gestión Operativa, Módulo de Registro de Equipos, Módulo de Gestión de Contratos, Gestión de Recursos Humanos, Facturación, Gestión de Documentos y Módulos de Reporte.';
-$lang['smartindustries_page_title3_p7'] = 'Operaciones Totales de Manejo de Carga tanto para puertos marítimos como para terminales de contenedores en tierra. Control de Contenedores, Manejo de Yarda, Planeamiento de Barcos carga de carga, Operaciones de Embarcaciones, Administración de Terminal, Control de Reefer, Planificación de Amarre, Facturación Automática y conjunto completo de Reportes.';
-$lang['smartindustries_page_title3_p8'] = 'Aquatrololis de Compta emerge para potenciar el sector acuícola con avances tecnológicos que permiten el control inteligente de todo el ciclo productivo, optimizando procesos de producción en línea con el comportamiento del mercado. Módulos disponibles: SmartMarket (Mercado de especies B2B y B2C), SmartFarm (Base y Trazabilidad), SmartEnvironment, SmartPower.';
-
-// Sección 5 (Tabla)
-$lang['smartindustries_section_features_h2'] = 'MÓDULOS, APLICABILIDAD E IMPACTOS';
-$lang['smartindustries_section_features_span'] = 'Para los Objetivos de Sostenibilidad y Eficiencia';
-$lang['smartindustries_table_th1'] = 'Módulos de soluciones';
-$lang['smartindustries_table_th2'] = 'Productos destacados';
-$lang['smartindustries_table_th3'] = 'Áreas de Aplicación';
-$lang['smartindustries_table_th4'] = 'Ahorro de energía';
-$lang['smartindustries_table_th5'] = 'Ahorro operacional';
-$lang['smartindustries_table_tr1_1'] = 'Gestión del <br>Consumo de Energía';
-$lang['smartindustries_table_tr2_1'] = 'Gestión de <br>Espacios Verdes';
-$lang['smartindustries_table_tr3_1'] = 'Gestión de <br>recolección de residuos';
-$lang['smartindustries_table_tr4_1'] = 'Agricultura';
-$lang['smartindustries_table_tr1_3_1'] = 'Iluminación pública';
-$lang['smartindustries_table_tr1_3_2'] = 'Iluminación exterior';
-$lang['smartindustries_table_tr1_3_3'] = 'Edificios de Oficinas y Campus';
-$lang['smartindustries_table_tr1_3_4'] = 'Condominios';
-$lang['smartindustries_table_tr1_3_5'] = 'Museos, Teatros y';
-$lang['smartindustries_table_tr1_3_6'] = 'Arenas';
-$lang['smartindustries_table_tr1_3_7'] = 'Piscinas';
-$lang['smartindustries_table_tr2_3_1'] = 'Parques y Jardines';
-$lang['smartindustries_table_tr2_3_2'] = 'Lagos';
-$lang['smartindustries_table_tr2_3_3'] = 'Cursos de golf';
-$lang['smartindustries_table_tr2_3_4'] = 'Universidades';
-$lang['smartindustries_table_tr2_3_5'] = 'Condominios';
-$lang['smartindustries_table_tr3_3_1'] = 'Optimización de rutas de camiones';
-$lang['smartindustries_table_tr3_3_2'] = 'Control predictivo de la máquina';
-$lang['smartindustries_table_tr3_3_3'] = 'Pagar';
-$lang['smartindustries_table_tr4_3_1'] = 'Control remoto de pivote';
-$lang['smartindustries_table_tr4_3_2'] = 'Goteos y rociadores';
-$lang['smartindustries_table_tr4_3_3'] = 'Boreholes & Bombas';
-$lang['smartindustries_table_tr4_3_4'] = 'Multi-cultivo';
+// Sección 3
+$lang['smartindustries_heading_block2_h1'] = 'BEE2 Product Family';
+$lang['smartindustries_heading_block2_img1'] = 'smartindustries-section3-img1.jpg';
+$lang['smartindustries_heading_block2_title1'] = 'Smart Cities and Industry Specific Verticals';
+$lang['smartindustries_heading_block2_p1'] = 'BEE2 Product Family is comprised today by 6 (six) IoT Vertical Applications: Bee2Energy, Bee2Lighting, Bee2Waste, Bee2Green, Bee2Crop and Bee2FireDetection, plus 1 (one) Service Application for Citizens: Bee2Citizen in form of a Mobile and Web App.';
+$lang['smartindustries_heading_block2_p2'] = 'The offer covers the most popular areas of interest for Smart City projects such as energy management, street lighting, urban waste collection, green spaces management and citizens plus other industry specific solutions for Agriculture and Forests such as smart irrigation and forest fire detection.';
